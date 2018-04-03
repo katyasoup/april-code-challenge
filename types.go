4659a6dd8df3fa2ca274dd7acde20c5dc7d7d731
katyasoup <campbell.katharine@gmail.com>
2018-04-03 11:07:34 -0700

    add messages.json and types.go

---
+++ types.go
@@ -0,0 +1,29 @@
+package main
+
+// Reservation data
+type Reservation struct {
+	Room     int `json:"roomNumber"`
+	ResStart int `json:"startTimestamp"`
+	ResEnd   int `json:"endTimestamp"`
+}
+
+// Guest data
+type Guest struct {
+	ID        int         `json:"id"`
+	FirstName string      `json:"firstName"`
+	LastName  string      `json:"lastName"`
+	ResDeets  Reservation `json:"reservation"`
+}
+
+// Company data
+type Company struct {
+	ID       int    `json:"id"`
+	Company  string `json:"company"`
+	City     string `json:"city"`
+	Timezone string `json:"timezone"`
+}
+
+// Message data
+type Message struct {
+	Message string `json:"message"`
+}
